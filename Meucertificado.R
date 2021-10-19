17b1517c1d93c675c9f784c818df51077f807fef
DATAUNIRIO <duttross@gmail.com>
2021-10-19 19:37:05 -0300

    Create Meucertificado.R

---
+++ Meucertificado.R
@@ -0,0 +1,19 @@
+
+
+
+gerar_certificados <- function(listadepresenca,modelocertificado) {
+  
+  ## Loop
+  for (i in 1:ncol(listadepresenca)){
+    presenca[,i]<-iconv(listadepresenca[,i], "latin1", "UTF-8")}
+  
+  ## Loop
+  for (i in 1:nrow(presenca)){
+    
+    rmarkdown::render(input = paste(modelocertificado),
+                      output_format = iconv("pdf_document", "latin1", "UTF-8"),
+                      output_file = paste("Certificado", i, ".pdf", sep=''),
+                      output_dir = "doc",
+                      encoding="UTF-8")
+  }
+}
